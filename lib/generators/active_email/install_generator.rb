1c169096738ddd6a27f872d4bfe0719932ea4a36
Carla Ares <carla.ares@gmail.com>
2013-09-17 20:26:16 -0300

    moving templates folder inside active_email generator

--- lib/generators/active_email/install_generator.rb
+++ lib/generators/active_email/install_generator.rb
@@ -1,8 +1,6 @@
 module ActiveEmail
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::ResourceHelpers
-
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_initializer
