498215a2b254ea1ba10b099f640efde8236039df
Carla Ares <carla.ares@gmail.com>
2013-09-17 22:46:06 -0300

    action mailer had 2 clases inside 1 module definition. we couldnt load it. fix it

--- lib/active_email/transactional/gateways/action_mailer.rb
+++ lib/active_email/transactional/gateways/action_mailer.rb
@@ -8,13 +8,19 @@ module ActiveEmail #:nodoc:
           to: "#{email.to_name} <#{email.to_email_address}>", 
           from: "#{email.from_name} <#{email.from_email_address}>",
           subject: email.subject,
-          delivery_method_options: delivery_options
+          body: email.html_content,
+          content_type: "text/html",
+          delivery_method_options: delivery_options,
+          return_response: true
         }.merge(email.reply_to.nil? ? {} : { "Reply-To" => email.reply_to })
 
         mail(options)
       end
     end
-
+  end
+end
+module ActiveEmail #:nodoc:
+  module Transactional #:nodoc:
     class ActionMailerGateway < Gateway
       # Specific to Mandril options:
       # * <tt>:account_type</tt> - Account type Gmail or Hotmail
@@ -29,6 +35,7 @@ module ActiveEmail #:nodoc:
       def initialize(options = {})
         begin
           require 'actionmailer'
+          require 'net/imap'
         rescue LoadError
           raise "Could not load the actionmailer gem (>= 4.0).  Use `gem install actionamiler` to install it."
         end
@@ -38,7 +45,7 @@ module ActiveEmail #:nodoc:
         if options[:account_type] == "gmail"
           address = "smtp.gmail.com"
           domain = "gmail.com"
-        elsif account_type == "hotmail"
+        elsif options[:account_type] == "hotmail"
           address = "smtp.live.com"
           domain = "hotmail.com"
         else
@@ -59,25 +66,36 @@ module ActiveEmail #:nodoc:
       end
 
       def status(transaction_id)
+
+        
         # Notifier.deliver_envio_mail_gmail(email, cuenta, template.description, template.body, template.attachement, template.attachement_file_name)
-#        build_response(:send, answer)
+        # build_response(:send, answer)
       end
 
       def send(email, options = {})
         # now this will send using custom SMTP settings
-        Notifier.send_email(email, options, @custom_smtp_settings).deliver
-        build_response(:send)
+        response = Notifier.send_email(email, options, @custom_smtp_settings).deliver
+
+        if @custom_smtp_settings[:domain] == "gmail.com"
+          imap = Net::IMAP.new('imap.gmail.com', 993, true)
+          imap.login(@custom_smtp_settings[:user_name], @custom_smtp_settings[:password])
+          imap.select('[Gmail]/Enviados')
+          imap.uid_search(["NOT", "DELETED", "SUBJECT", email.subject, "TO", email.to_email_address])
+        end
+
+        build_response(:send, nil, id)
       rescue
         build_response(:send, $!)
       end
 
       private
-        def build_response(kind, response = nil)
+        def build_response(kind, response = nil, id = nil)
           Response.new(
               response,
               response.nil?,
               response, 
-              { :test => test? }
+              { :transaction_id => id,
+                :test => test? }
           )
         end
     end
