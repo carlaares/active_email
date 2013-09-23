66f15bd02ec72be5071f9fa0da46cf034118096a
Carla Ares <carla.ares@gmail.com>
2013-09-23 14:49:39 -0300

    active record generator table name error. fixed

--- lib/generators/active_email/templates/webhooks_received.rb
+++ lib/generators/active_email/templates/webhooks_received.rb
@@ -1,6 +1,6 @@
 class WebhooksReceived < ActiveRecord::Migration
   def change
-    create_table :delayed_jobs, :force => true do |table|
+    create_table :webhooks, :force => true do |table|
       table.string  :event
       table.string  :transaction_id
       table.text     :raw_message
